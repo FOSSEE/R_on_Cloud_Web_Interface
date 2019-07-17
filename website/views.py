76dcbadbf71febe93fb9d2ffd51b2f3ae54233a2
prashantsinalkar <prashantsinalkar@gmail.com>
2019-07-17 14:56:54 +0530

    added file select validation and varible for upload file api url

--- website/views.py
+++ website/views.py
@@ -3,11 +3,13 @@ from django.http import HttpResponse
 from django.template import loader
 import requests
 import uuid
+from R_on_Cloud.config import (API_URL_UPLOAD)
 
 def index(request):
     context = {}
     session_id = uuid.uuid4()
     context['session_id'] = str(session_id)
+    context['api_url_upload'] = API_URL_UPLOAD
     request.session['session_id'] = str(session_id)
     template = loader.get_template('index.html')
     return HttpResponse(template.render(context, request))
