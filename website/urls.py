2fe8087068dda1729115fc245f1fae6796a4363e
prashantsinalkar <prashantsinalkar@gmail.com>
2019-09-07 23:47:54 +0530

    added new urls for get data

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 
 from . import views
+from . import ajax
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('index', views.index, name='index'),
+    path('get_subcategories/', ajax.subcategories,
+        name='subcategories'),
+    path('get_books/', ajax.books, name='books'),
+    path('get_chapters/', ajax.chapters, name='chapters'),
+    path('get_examples/', ajax.examples, name='examples'),
+    path('get_revisions/', ajax.revisions, name='revisions'),
+    path('get_code/', ajax.code, name='code'),
+    path('get_diff/', ajax.diff, name='diff'),
+    path('update_view_count/', views.update_view_count,
+        name='update_view_count')
     ]
